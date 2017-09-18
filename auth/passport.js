95bfb5f82a8d1e2d58118c92c1149b9845193810
Saurabh <saurabh.verma2892@gmail.com>
2017-09-18 02:23:12 -0500

    asd

--- auth/passport.js
+++ auth/passport.js
@@ -95,7 +95,7 @@ module.exports = app => {
             // pull in our app id and secret from our auth.js file
             clientID        : '1927644547501515',
             clientSecret    : '0e46ee3057e144d7c0fea47c8e012241',
-            callbackURL     : 'http://localhost:3001/voter/facebook/callback',
+            callbackURL     : 'http://seatmx.com:3001/voter/facebook/callback',
             passReqToCallback : true,
             profileFields: ['id', 'email', 'displayName','hometown','location'],
             scope: ['email','user_location','user_hometown']
